74f04217c04e3ce7672942a928c9d76de754f7b7
TobsenD <tobias.dols@outlook.com>
2023-11-15 18:45:51 +0100

    Update to neoforge 1.20.2-20.2.35-beta

--- src/main/java/net/tobsend/siegestopper/SiegeStopper.java
+++ src/main/java/net/tobsend/siegestopper/SiegeStopper.java
@@ -5,15 +5,15 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import net.minecraft.world.entity.ai.village.VillageSiege;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.event.village.VillageSiegeEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.village.VillageSiegeEvent;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(SiegeStopper.MODID)
@@ -31,8 +31,8 @@ public class SiegeStopper {
     modEventBus.addListener(this::commonSetup);
 
     // Register ourselves for server and other game events we are interested in
-    MinecraftForge.EVENT_BUS.register(this);
-    MinecraftForge.EVENT_BUS.register(SiegeEventHandler.class);
+    NeoForge.EVENT_BUS.register(this);
+    NeoForge.EVENT_BUS.register(SiegeEventHandler.class);
   }
 
   private void commonSetup(final FMLCommonSetupEvent event) {}
